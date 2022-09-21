f5d2ac6a3d00f52d067386480a3dc4017a88c00a
Ryo802701 <rkawa75@gmail.com>
2022-09-21 20:56:29 +0900

    first commit

---
+++ src/main/java/com/example/RaiseTechJavaEx9/RaiseTechJavaEx9Application.java
@@ -0,0 +1,13 @@
+package com.example.RaiseTechJavaEx9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaiseTechJavaEx9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaiseTechJavaEx9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RaiseTechJavaEx9/RaiseTechJavaEx9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RaiseTechJavaEx9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaiseTechJavaEx9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
